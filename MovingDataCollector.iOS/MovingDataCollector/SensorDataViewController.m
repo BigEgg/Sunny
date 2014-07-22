9841d0a17900a8ed26e8d15504e8f5e7601ccb0c
Jianming Xiao <Jianming.Xiao@akqa.com>
2014-07-22 21:08:51 +0800

    Abstract the initialize of motion manager, and set the default view for the section view property.

--- MovingDataCollector.iOS/MovingDataCollector/SensorDataViewController.m
+++ MovingDataCollector.iOS/MovingDataCollector/SensorDataViewController.m
@@ -29,47 +29,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
-    motionManager = [[CMMotionManager alloc] init];
-    motionManager.accelerometerUpdateInterval = 1.0 / 5.0; //  Update at 5Hz
-    motionManager.gyroUpdateInterval = 1.0 / 5.0; //  Update at 5Hz
-    
-    if (motionManager.accelerometerAvailable)
-    {
-        accelerometerQueue = [NSOperationQueue currentQueue];
-        [motionManager startAccelerometerUpdatesToQueue: accelerometerQueue
-                                            withHandler:^(CMAccelerometerData *accelerometerData, NSError *error)
-                                                {
-                                                    CMAcceleration acceleration = accelerometerData.acceleration;
-
-                                                    _xAccelerometerLabel.text = [NSString stringWithFormat:@"%f", acceleration.x];
-                                                    _xAccelerometerBar.progress = ABS(acceleration.x);
-
-                                                    _yAccelerometerLable.text = [NSString stringWithFormat:@"%f", acceleration.y];
-                                                    _yAccelerometerBar.progress = ABS(acceleration.y);
-
-                                                    _zAccelerometerLable.text = [NSString stringWithFormat:@"%f", acceleration.z];
-                                                    _zAccelerometerBar.progress = ABS(acceleration.z);
-                                                }];
-    }
-    if (motionManager.gyroAvailable)
-    {
-        NSLog(@"Gyroscope avaliable.");
-        gyroscopeQueue = [NSOperationQueue currentQueue];
-        [motionManager startGyroUpdatesToQueue: gyroscopeQueue
-                                   withHandler:^(CMGyroData *gyroData, NSError *error)
-                                                {
-                                                    CMRotationRate rotate = gyroData.rotationRate;
-
-                                                    _xGyroscopeLable.text = [NSString stringWithFormat:@"%f", rotate.x];
-                                                    _xGyroscopeBar.progress = ABS(rotate.x);
-
-                                                    _yGyroscopeLable.text = [NSString stringWithFormat:@"%f", rotate.y];
-                                                    _yGyroscopeBar.progress = ABS(rotate.y);
-
-                                                    _zGyroscopeLable.text = [NSString stringWithFormat:@"%f", rotate.z];
-                                                    _zGyroscopeBar.progress = ABS(rotate.z);
-                                                }];
-    }
+    [self.sectionView addSubview:self.accelerometerView];
+    [self initializeMotionManager];
 }
 
 - (void)didReceiveMemoryWarning
@@ -86,5 +47,52 @@
         [self.sectionView addSubview:self.accelerometerView];
     else if (sectionControl.selectedSegmentIndex == 1)  // Progress section
         [self.sectionView addSubview:self.gyroscopeView];
+}
+
+#pragma mark - Private Methods
+- (void)initializeMotionManager
+{
+    motionManager = [[CMMotionManager alloc] init];
+    motionManager.accelerometerUpdateInterval = 1.0 / 5.0; //  Update at 5Hz
+    motionManager.gyroUpdateInterval = 1.0 / 5.0; //  Update at 5Hz
+    
+    if (motionManager.accelerometerAvailable)
+    {
+        accelerometerQueue = [NSOperationQueue currentQueue];
+        [motionManager startAccelerometerUpdatesToQueue: accelerometerQueue
+                                            withHandler:^(CMAccelerometerData *accelerometerData, NSError *error)
+         {
+             CMAcceleration acceleration = accelerometerData.acceleration;
+             
+             _xAccelerometerLabel.text = [NSString stringWithFormat:@"%f", acceleration.x];
+             _xAccelerometerBar.progress = ABS(acceleration.x);
+             
+             _yAccelerometerLable.text = [NSString stringWithFormat:@"%f", acceleration.y];
+             _yAccelerometerBar.progress = ABS(acceleration.y);
+             
+             _zAccelerometerLable.text = [NSString stringWithFormat:@"%f", acceleration.z];
+             _zAccelerometerBar.progress = ABS(acceleration.z);
+         }];
+    }
+    if (motionManager.gyroAvailable)
+    {
+        NSLog(@"Gyroscope avaliable.");
+        gyroscopeQueue = [NSOperationQueue currentQueue];
+        [motionManager startGyroUpdatesToQueue: gyroscopeQueue
+                                   withHandler:^(CMGyroData *gyroData, NSError *error)
+         {
+             CMRotationRate rotate = gyroData.rotationRate;
+             
+             _xGyroscopeLable.text = [NSString stringWithFormat:@"%f", rotate.x];
+             _xGyroscopeBar.progress = ABS(rotate.x);
+             
+             _yGyroscopeLable.text = [NSString stringWithFormat:@"%f", rotate.y];
+             _yGyroscopeBar.progress = ABS(rotate.y);
+             
+             _zGyroscopeLable.text = [NSString stringWithFormat:@"%f", rotate.z];
+             _zGyroscopeBar.progress = ABS(rotate.z);
+         }];
+    }
+    
 }
 @end

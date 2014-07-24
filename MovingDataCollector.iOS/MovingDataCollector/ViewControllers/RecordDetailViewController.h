d5de4fabd642d1a409b64be8efd9fd2777e2fb22
Jianming Xiao <Jianming.Xiao@akqa.com>
2014-07-24 17:05:07 +0800

    Add the Record Detail view.

---
+++ MovingDataCollector.iOS/MovingDataCollector/ViewControllers/RecordDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecordDetailViewController.h
+//  MovingDataCollector
+//
+//  Created by Jianming Xiao on 7/24/14.
+//  Copyright (c) 2014 jianming.xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecordDetailViewController : UIViewController
+
+@end
---
+++ MovingDataCollector.iOS/MovingDataCollector/ViewControllers/RecordDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  RecordDetailViewController.m
+//  MovingDataCollector
+//
+//  Created by Jianming Xiao on 7/24/14.
+//  Copyright (c) 2014 jianming.xiao. All rights reserved.
+//
+
+#import "RecordDetailViewController.h"
+
+@interface RecordDetailViewController ()
+
+@end
+
+@implementation RecordDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

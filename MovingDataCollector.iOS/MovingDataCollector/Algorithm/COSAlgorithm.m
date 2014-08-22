4eeb5a90b0a27196b514cbc8f2e77804675bc48c
Jianming Xiao <Jianming.Xiao@akqa.com>
2014-08-22 12:08:09 +0800

    Still need to calculate the length of each vector.

--- MovingDataCollector.iOS/MovingDataCollector/Algorithm/COSAlgorithm.m
+++ MovingDataCollector.iOS/MovingDataCollector/Algorithm/COSAlgorithm.m
@@ -12,7 +12,11 @@
 @implementation COSAlgorithm
 
 - (float)computeWithData:(AccelerometerData *)data andOldData:(AccelerometerData *)oldData {
-    return (data.x * oldData.x + data.y * oldData.y + data.z * oldData.z);
+    float lenght1 = sqrtf(powf(data.x, 2) + powf(data.y, 2) + powf(data.z, 2));
+    float lenght2 = sqrtf(powf(oldData.x, 2) + powf(oldData.y, 2) + powf(oldData.z, 2));
+    
+    return (data.x * oldData.x + data.y * oldData.y + data.z * oldData.z) / (lenght1 * lenght2);
+
 }
 
 @end

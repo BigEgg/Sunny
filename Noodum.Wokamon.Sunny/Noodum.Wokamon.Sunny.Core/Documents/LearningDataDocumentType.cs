18041fe18d517727bbc50c42743cacb5db600034
billmoling <panan.songyu@gmail.com>
2014-08-23 18:10:32 +0800

    Add the learning document. 

---
+++ Noodum.Wokamon.Sunny/Noodum.Wokamon.Sunny.Core/Documents/LearningDataDocumentType.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Noodum.Wokamon.Sunny.Core.Documents
+{
+    public class LearningDataDocumentType
+    {
+        #region Fields
+        private const string fileExtension = ".learning";
+        private static string fileRootPath;
+        #endregion
+
+        /// <summary>
+        /// Initializes the <see cref="SensorDataDocumentType"/> class.
+        /// </summary>
+        static LearningDataDocumentType()
+        {
+            fileRootPath = Path.Combine(
+                Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory),
+                "Learning");
+        }
+
+        #region Properties
+        /// <summary>
+        /// Gets the file root path.
+        /// </summary>
+        /// <value>
+        /// The file root path.
+        /// </value>
+        public static string FileRootPath { get { return fileRootPath; } }
+        #endregion
+
+
+    }
+}
